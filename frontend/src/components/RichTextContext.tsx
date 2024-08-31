1c7abbcd1727d4f67b4de2f7c494044e97222f52
deewakar-k <dakshftw01@gmail.com>
2024-08-31 14:13:02 +0530

    v1 complete

--- frontend/src/components/RichTextContext.tsx
+++ frontend/src/components/RichTextContext.tsx
@@ -1,16 +1,25 @@
 import { useEffect } from "react";
 import hljs from "highlight.js";
 import "highlight.js/styles/github-dark.css";
+import "../index.css";
+
+const convertEmptyParagraphs = (html: string): string => {
+  return html
+    .replace(/<p>\s*<\/p>/g, "<br/>") // Convert empty paragraphs to <br/>
+    .replace(/<\/p>\s*<p>/g, "</p><br/><p>"); // Add <br/> between paragraphs
+};
 
 export const RichTextContext = ({ content }: { content: string }) => {
   useEffect(() => {
     hljs.highlightAll();
   }, []);
 
+  const formattedContent = convertEmptyParagraphs(content);
+
   return (
     <div
       className="tiptap-content"
-      dangerouslySetInnerHTML={{ __html: content }}
+      dangerouslySetInnerHTML={{ __html: formattedContent }}
     ></div>
   );
 };
